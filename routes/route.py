63e5102ffde4641b40d7318746af4a02d38f34f8
Isaac <asabaisaac1@gmail.com>
2024-10-09 21:07:41 +0200

    worked on post hook

--- routes/route.py
+++ routes/route.py
@@ -5,9 +5,16 @@ from bson import ObjectId
 
 from schema.schema import list_serial
 
-router =APIRouter()
+router = APIRouter()
+
 
 @router.get("/")
 async def get_todo():
     todos = list_serial(collection_name.find())
-        return todos
+    return todos
+
+
+@router.post("/")
+async def post_todo(todo: Todos):
+    collection_name.insert_one(dict.find)
+
