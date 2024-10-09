2f6cfd6b63b4f1b6a5cf163b42f88f010f42112d
Isaac <asabaisaac1@gmail.com>
2024-10-09 20:23:41 +0200

    updated files in routes

---
+++ routes/route.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from models.todos import Todos
+from config.database import collection_name
+from bson import ObjectId
+
+from schema.schema import list_serial
+
+router =APIRouter()
+
+@router.get("/")
+async def get_todo():
+    todos = list_serial(collection_name.find())
+        return todos
--- schema/schema.py
+++ schema/schema.py
@@ -1,13 +1,16 @@
-#json Serializer
+# json Serializer
 from models import todos
 
-def individual_serial(todo)->dict:
-    return {"id":str(todo["_id"]),
-            "name":todo["_name"],
-            "description":todo["_description"],
-            "complete":todo["complete"]
 
-    }
-#deserialiser
-def list_serial(todo)->list:
+def individual_serial(todo) -> dict:
+    return {"id": str(todo["_id"]),
+            "name": todo["_name"],
+            "description": todo["_description"],
+            "complete": todo["complete"]
+
+            }
+
+
+# deserialiser
+def list_serial(todo) -> list:
     return [individual_serial(todo) for todo in todos]
