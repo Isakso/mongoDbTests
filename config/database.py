e54036d86ef573747c44868febce48ea8998feb3
Isaac <asabaisaac1@gmail.com>
2024-10-14 18:30:50 +0200

    migration succeded

--- config/database.py
+++ config/database.py
@@ -1,8 +1,9 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
-
-
-client = MongoClient("mongodb+srv://root:******@cluster0.3lgpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-db = client.todo_db
-collection_name = db["todo_collections"]
+try:
+    client = MongoClient("mongodb+srv://root:****@cluster0.3lgpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    db = client.todo_db
+    collection_name = db["todo_collections"]
+    print("Connected to the database successfully!")
+except errors.ConnectionError as e:
+    print(f"Could not connect to the database: {e}")
