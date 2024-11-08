000d70457d4ae1e450337fcb098f1e52412a60de
Isaac <88895507+Isakso@users.noreply.github.com>
2024-11-08 13:10:05 +0100

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,9 +1,26 @@
-from pymongo import MongoClient, errors
+from email import errors
+from dotenv import load_dotenv
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+import os
+
+
+
+
+load_dotenv()
+
+username = os.getenv("MONGO_USER")
+password = os.getenv("MONGO_PASS")
+db_name = os.getenv("MONGO_DB_NAME")
+collection_name_str = os.getenv("MONGO_COLLECTION_NAME")
+
 
 try:
-    client = MongoClient("mongodb+srv://root****@cluster0.3lgpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    db = client.todo_db
-    collection_name = db["todo_collections"]
+
+    client = AsyncIOMotorClient(f"mongodb+srv://{username}:{password}@cluster0.3lgpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    db = client[db_name]
+    collection_name = db[collection_name_str]
     print("Connected to the database successfully!")
-except errors.ConnectionError as e:
+except Exception as e:
     print(f"Could not connect to the database: {e}")
